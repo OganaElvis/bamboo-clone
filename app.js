77368fa3556dc3e38673050786db7638731c66b8
OganaElvis <125921202+OganaElvis@users.noreply.github.com>
2023-04-09 02:36:08 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// let menuIcon = document.getElementById('menu-icon');
+// let listContainer = document.getElementById('list-container');
+
+// menuIcon.addEventListener('click', ()=>{
+//     listContainer.style.display="block";
+// })
